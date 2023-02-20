14f315c854c29cfc2121208cd2c85c4fdf89ab5a
gleeford <mgmromasanta@gmail.com>
2023-02-20 11:18:12 +0800

    first commit

---
+++ Udemy/Project 4 Calculator/script.js
@@ -0,0 +1,13 @@
+function clr() {
+  document.getElementById("textVal").value = "";
+}
+function display(val) {
+  document.getElementById("textVal").value =
+    document.getElementById("textVal").value + val;
+}
+
+function calculate() {
+  let x = document.getElementById("textVal").value;
+  let y = eval(x);
+  document.getElementById("textVal").value = y;
+}
